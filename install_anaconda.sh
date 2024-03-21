573d606d97cadb02fc7f03d34b5640077ae02eea
jpmalek <jeff.malek@gmail.com>
2024-03-21 15:49:02 -0700

    Update Anaconda installation script

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -3,8 +3,9 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 # The anaconda-navigator package is installed by default with Anaconda Distribution and includes the 
 # dependency package qt. All of the dependencies for qt should also be installed by default, but installing
 # the following extended dependencies might be required for certain versions of Anaconda Distribution in order to 
-# use GUI (Graphical User Interface) packages with Linux:
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+# use GUI (Graphical User Interface) packages with Linux.
+# Uncomment the next line if the GUI is needed.
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 # Replace <INSTALLER_VERSION> with the version of the installer file you want to download
 # For example, https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 # All installers can be found at repo.anaconda.com/archive/
@@ -23,5 +24,5 @@ fi
 # Replace ~/Downloads with the path to the installer file, if necessary
 # Replace <INSTALLER_VERSION> with the version of the installer file
 # For example, Anaconda3-2023.09-0-Linux-x86_64.sh
-bash ~/Anaconda3-2024.02-1-Linux-x86_64.sh
+bash Anaconda3-2024.02-1-Linux-x86_64.sh  -b -p $HOME/anaconda3
 source ~/.bashrc
