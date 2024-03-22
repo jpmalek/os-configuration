89b48affaf74e6da6d97448a52f45d018046776d
jpmalek <jeff.malek@gmail.com>
2024-03-21 17:29:15 -0700

    Add variable to capture current directory

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo apt-get upgrade -y
+# capture the current directory in a variable for use in executing scripts in the same directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Anaconda takes many GB of space! So for now, commenting this out. Script is ready to go, just double-check latest version of Anaconda
 # .install_anaconda.sh
-. install_standard_packages.sh
-. install_chrome.sh
-. install_awscli.sh
-. install_docker.sh
+. $DIR/install_standard_packages.sh
+. $DIR/install_chrome.sh
+. $DIR/install_awscli.sh
+. $DIR/install_docker.sh
